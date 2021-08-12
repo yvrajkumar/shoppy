679c35dc5fd139cc0ad71fab81e93475af68aaf0
yvrajkumar <rajkumaryvrk@gmail.com>
2021-08-12 11:39:59 +0530

    changes made for searchfeature filters

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,13 +5,13 @@ class SearchController < ApplicationController
             category = params[:search]
             size = params[:size]  
             color = params[:color]
-            @products = Search.where('lower(gender) = lower(?)', gender).and(Search.where('lower(size) like lower(?)', "%#{size}%")).and(Search.where('lower(category) like lower(?)', "%#{category}%")).and(Search.where('lower(searchfilters) like lower(?)', "%#{color}%")).and(Search.where("lower(searchfilters) LIKE lower(?) ", "%#{category}%"))
+            @products = Search.where('lower(gender) = lower(?)', gender).and(Search.where('lower(size) like lower(?)', "%#{size}%")).and(Search.where('lower(searchfilters) like lower(?)', "%#{color}%")).and(Search.where("lower(searchfilters) LIKE lower(?) ", "%#{category}%"))
             puts @products
         elsif !params[:size].blank? 
             gender = params[:gender]
             category = params[:search]
             size = params[:size]  
-            @products = Search.where('lower(gender) = lower(?)', gender).and(Search.where('lower(size) like lower(?)', "%#{size}%")).or(Search.where('lower(category) like lower(?)', "%#{category}%")).or(Search.where("lower(img_url) LIKE lower(?) ", "%#{category}%")).or(Search.where("lower(searchfilters) LIKE lower(?) ", "%#{category}%"))
+            @products = Search.where('lower(gender) = lower(?)', gender).and(Search.where('lower(size) like lower(?)', "%#{size}%")).and(Search.where("lower(searchfilters) LIKE lower(?) ", "%#{category}%"))
             puts @products
         elsif !params[:color].blank? 
             gender = params[:gender]
